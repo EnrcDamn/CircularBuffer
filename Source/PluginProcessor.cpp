af71c354c9603e27f2fb3a8923a9191461c3ca6f
EnrcDamn <enrico.damiani10@gmail.com>
2022-04-30 12:36:13 +0200

    Implemented circular buffer

---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,206 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+DelayAudioProcessor::DelayAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+}
+
+DelayAudioProcessor::~DelayAudioProcessor()
+{
+}
+
+//==============================================================================
+const juce::String DelayAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool DelayAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool DelayAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool DelayAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double DelayAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int DelayAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int DelayAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void DelayAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String DelayAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void DelayAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void DelayAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    auto delayBufferSize = 2.0 * sampleRate;
+    delayBuffer.setSize(getTotalNumOutputChannels(), (int)delayBufferSize);
+}
+
+void DelayAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool DelayAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void DelayAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    auto bufferSize = buffer.getNumSamples();
+    auto delayBufferSize = delayBuffer.getNumSamples();
+    
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        auto* channelData = buffer.getWritePointer (channel);
+
+        fillBuffer(channel, bufferSize, delayBufferSize, channelData);
+    }
+    DBG("Delay Buffer Size: " << delayBufferSize);
+    DBG("Delay Buffer: " << bufferSize);
+    DBG("Write Position: " << writePosition);
+
+    writePosition += bufferSize;
+    writePosition %= delayBufferSize;
+}
+
+void DelayAudioProcessor::fillBuffer(int channel, int bufferSize, int delayBufferSize, float* channelData)
+{
+    // if we can copy main buffer without needing to wrap
+    if (delayBufferSize > bufferSize + writePosition)
+    {
+        delayBuffer.copyFromWithRamp(channel, writePosition, channelData, bufferSize, 0.1f, 0.1f);
+    }
+    else
+    {
+        auto numSamplesToEnd = delayBufferSize - writePosition;
+        delayBuffer.copyFromWithRamp(channel, writePosition, channelData, numSamplesToEnd, 0.1f, 0.1f);
+
+        auto numSamplesFromStart = bufferSize - numSamplesToEnd;
+        delayBuffer.copyFromWithRamp(channel, 0, channelData + numSamplesToEnd, numSamplesFromStart, 0.1f, 0.1f);
+    }
+}
+
+//==============================================================================
+bool DelayAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* DelayAudioProcessor::createEditor()
+{
+    return new DelayAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void DelayAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void DelayAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new DelayAudioProcessor();
+}
